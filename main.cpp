f5158b591b3c354deb4abaa2d936eac19a87d819
Angus Hutton-McKenzie <a.a.hutton-mckenzie@outlook.com>
2023-03-21 20:02:25 +0000

    Update

--- main.cpp
+++ main.cpp
@@ -7,7 +7,13 @@ BufferedSerial xbee(PA_9,PA_10, 115200);
 
 Thread readThread;
 
+char startByte = 0x7E;
+
+
 char buf[BUFFERSIZE] = {0};
+char temp[BUFFERSIZE] = {0};
+char buffer[BUFFERSIZE] = {0};
+int length;
 
 void reader()
 {
@@ -16,7 +22,7 @@ void reader()
         if (uint32_t num = xbee.read(buf, sizeof(buf))) {
             // Toggle the LED.
             //led = !led;
-
+            //length = snprintf(temp,BUFFERSIZE,"%s",buf);
             // Echo the input back to the terminal.
             pc.write(buf, num);
         }
@@ -38,8 +44,12 @@ int main()
     );
     readThread.start(reader);
 
-    while (true) {
+    int i = (0xFF - (0x10 + 0x01 + 0xFF + 0xFF + 0xFF + 0xFF + 0xFF + 0xFF + 0xFF + 0xFF + 0xFF + 0xFE + 0x00 + 0x00 + 0x41));
+        length = snprintf(buffer, BUFFERSIZE, "%x", i);
+        pc.write(buffer, length);
 
+    while (true) {
+        ThisThread::sleep_for(chrono::seconds(10));
     }
 }
 
